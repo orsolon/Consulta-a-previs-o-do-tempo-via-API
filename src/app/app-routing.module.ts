1a08d66f29f7637b56d4a23a01dab33b335de73f
orsolon <56239735+orsolon@users.noreply.github.com>
2021-04-13 15:40:31 -0300

    commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import * as core from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { BookmarksPage } from './pages/bookmarks/containers/bookmarks/bookmarks.page';
+import { HomePage } from './pages/home/containers/home/home.page';
+
+const routes: Routes = [
+  { path: '', component: HomePage },
+  { path: 'bookmarks', component: BookmarksPage },
+  { path: 'details', loadChildren: () => import('./pages/details/details.module').then(m => m.DetailsModule) },
+];
+
+@core.NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiKey: '65e5438d7093d8e56b80c2f9cda97a5b',
+};
